809f82316f5945f24bc6fb33e73e255a99b474a5
Guillermo Alfaro <galfarotolon@gmail.com>
2020-07-21 10:25:58 -0500

    added logo to the navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Row from './components/Row.js';
 import requests from './requests.js';
 import Banner from './components/Banner.js'
+import Nav from './components/Nav.js'
 
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Welcome to Movie Master!</h1> */}
+      <Nav />
       <Banner />
       <Row title="Netflix Originals" url={requests.fetchNetflixOriginals} largeRow></Row>
       <Row title="Trending Now" url={requests.fetchTrending}></Row>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
+import logo from '../Logo_1.png'
+
+
 
 
 
 function Nav() {
     return (
         <div className='nav'>
+            <img src={logo} alt="movie-master" />
 
         </div>
     )
 }
 
 export default Nav
+
+
