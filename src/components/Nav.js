621e6b307e189eb6a50c277d830d7b395723e73d
Guillermo Alfaro <galfarotolon@gmail.com>
2020-07-21 10:53:06 -0500

    added more styling to navbar and a fade effect on scroll down

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import logo from '../Logo_1.png'
 
@@ -7,9 +7,32 @@ import logo from '../Logo_1.png'
 
 
 function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 450) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+
+        return () => {
+            window.removeEventListener('scroll');
+        }
+
+    }, [])
+
+    // if show is true (if scrolled passed 100 px, then append navBlack class)
     return (
-        <div className='nav'>
-            <img src={logo} alt="movie-master" />
+        <div className={`nav ${show && 'navBlack'} `}>
+            <img className='navLogo' src={logo} alt="movie-master" />
+            <img className='navAvatar' src={logo} alt="movie-master" />
+
+
 
         </div>
     )
